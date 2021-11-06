7ab198809551426b230437c227e3e2ddc6cf2061
alice1321 <92432079+alice1321@users.noreply.github.com>
2021-11-06 15:05:54 +0100

    Add files via upload

---
+++ Robot control/register_data.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+
+import rospy
+import csv
+from geometry_msgs.msg import Vector3, PointStamped
+from rospy.names import global_name
+from lwr_controllers.msg import PoseRPY
+
+
+def get_tool_pose(tool):
+    global tool_pose
+    tool_pose = tool
+
+def get_actual_position(position):
+    global actual_position
+    actual_position = position
+
+def get_global_position(glob):
+    global global_position 
+    global_position = glob
+
+filename = rospy.get_param("/file_name")  #use rosparam set in terminal: rosparam set file_name 'file.csv'
+
+def main():
+    global tool_pose
+    global actual_position
+    global global_position
+    rospy.init_node('data_recording', anonymous= True)
+    rospy.Subscriber('/tool_pose', PointStamped, get_tool_pose)
+    rospy.Subscriber('/cartesian_position/pose/tooltip', PoseRPY, get_actual_position)
+    rospy.Subscriber('/lwr/one_task_inverse_kinematics/command', PoseRPY, get_global_position)
+    rate = rospy.Rate(10)
+    file = open(filename, 'w')
+    fieldnames = ['time','detection_time', 'local_position_x','local_position_y', 'local_position_z','global_position_x','global_position_y', 'global_position_z', 'actual_position_x','actual_position_y', 'actual_position_z']
+    writer = csv.DictWriter(file, fieldnames=fieldnames, dialect='excel')
+    writer.writeheader()
+    rospy.sleep(1)
+
+
+    while not rospy.is_shutdown():
+        global tool_pose
+        global actual_position
+        global global_position
+        time = rospy.Time.now()
+        writer.writerow({'time': time,'detection_time': tool_pose.header.stamp, 'local_position_x': tool_pose.point.x, 'local_position_y': tool_pose.point.y, 'local_position_z': tool_pose.point.z, 'global_position_x': global_position.position.x ,'global_position_y': global_position.position.y, 'global_position_z':global_position.position.z, 'actual_position_x': actual_position.position.x, 'actual_position_y': actual_position.position.y, 'actual_position_z': actual_position.position.z})
+        rate.sleep()
+
+    file.close()
+
+if __name__ == '__main__':
+	main()
+
+
+
+
