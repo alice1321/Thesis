055f2db50c87e86408c0ada9cc5bb1d3f25802ca
alice1321 <92432079+alice1321@users.noreply.github.com>
2021-12-28 11:40:09 +0100

    Update send_position.py

--- Robot control/send_position.py
+++ Robot control/send_position.py
@@ -21,24 +21,19 @@ yaw = -3.11634577067
 initial_position = Vector3(-0.414228491773, 0.390018906215 ,0.18561169506)
 initial_orientation = RPY(roll, pitch, yaw)
 
-
-'''cartesian_transform = transformations.euler_matrix(roll, pitch, yaw, axes ='sxyz')
-cartesian_transform[0,3] = initial_position.x
-cartesian_transform[1,3] = initial_position.y
-cartesian_transform[2,3] = initial_position.z'''
-
-
-def get_desired_position(desired):
+#Callbacks
+def get_desired_position(ros_data):
     global position
-    position = desired
+    position = ros_data
 
-def get_actual_position(pose):
+def get_actual_position(ros_data):
     global actual_position 
     global actual_orientation
-    actual_position = pose.position
-    actual_orientation = pose.orientation
+    actual_position = ros_data.position
+    actual_orientation = ros_data.orientation
 
-
+	
+#Main
 def main():
     rospy.init_node('send_pose', anonymous=True)
     rospy.Subscriber('/tool_pose', PointStamped, get_desired_position)
@@ -46,7 +41,7 @@ def main():
     publisher = rospy.Publisher('/lwr/one_task_inverse_kinematics/command', PoseRPY, queue_size=10)
     position_publish = rospy.Publisher('position', PointStamped, queue_size=10)
     rate = rospy.Rate(10)
-    rospy.sleep(1)
+    #rospy.sleep(1)
 
     command = PoseRPY(0, initial_position, initial_orientation) #set initial position
     publisher.publish(command)
@@ -56,56 +51,35 @@ def main():
     listener = TransformListener()
     listener.waitForTransform('lwr_base_link', 'lwr_7_link', now, rospy.Duration(1.0))
     final_position = Vector3()
-    final_position.z = 0.18561169506  #to be set
+    #final_position.z = 0.18561169506  #keep a fixed z position
     #(trans, rot) = listener.lookupTransform('lwr_base_link', 'lwr_7_link', rospy.Time(0))
     #transform = listener.fromTranslationRotation(trans, rot)
 
     while not rospy.is_shutdown():
 
-	
-	#point = transformations.translation_matrix((position.x, position.y, position.z)) #real point
 #--------------use listener transform point----------
-	wait = position.header.stamp + rospy.Duration(0.3)
+	wait = position.header.stamp  #wait for the transform at the detection time to become available
 	listener.waitForTransform('lwr_base_link', 'lwr_7_link', wait, rospy.Duration(2.0))
 	position_base = listener.transformPoint('lwr_base_link', position)
-	position_base.header.stamp = rospy.Time.now()
+	position_base.header.stamp = rospy.Time.now()  #used to record global position in time
 	position_publish.publish(position_base)
 	final_position.x = position_base.point.x
 	final_position.y = position_base.point.y
-	final_position.z = position_base.point.z + 0.1
+	final_position.z = position_base.point.z + 0.2  #keep the camera 20 cm far from the instrument
 	
-#----------------use transform------
-        '''(trans, rot) = listener.lookupTransform('lwr_base_link', 'lwr_7_link', rospy.Time(0))
+#----------------use transform------------------------
+        '''(trans, rot) = listener.lookupTransform('lwr_base_link', 'lwr_7_link', rospy.Time(0))  #gives the last available transform (not synchronised with the detection)
 	transform = listener.fromTranslationRotation(trans, rot)  #update transform
 	position_new = np.dot(transform, np.array([position.point.x, position.point.y, position.point.z, 1]))
 	final_position.x = position_new[0]
         final_position.y = position_new[1]
         final_position.z = position_new[2] + 0.1
 	print('new position transform',position_new)'''
-#-----------------------------------
+#------------------------------------------------------
 
-#----------------use tooltip---------
-	
-	'''roll = actual_orientation.roll
-	pitch = actual_orientation.pitch
-	yaw = actual_orientation.yaw
-	cartesian_transform = transformations.euler_matrix(roll, pitch, yaw, axes ='sxyz')
-	cartesian_transform[0,3] = actual_position.x
-	cartesian_transform[1,3] = actual_position.y
-	cartesian_transform[2,3] = actual_position.z   #update transform
-
-	position_new = np.dot(T, point[:,-1])
-	position_new = np.dot(cartesian_transform, position_new)
-	print('new position tooltip', position_new)'''
-
-#--------------------------------------------------------
-	
-	command_old = command
-	command = PoseRPY(0, final_position, initial_orientation)
-	'''if command != command_old  #too many commands sent
-		if abs(actual_position.x - final_position.x) < 0.15 and abs(actual_position.y - final_position.y) < 0.1:
-	    		publisher.publish(command)'''
-	print('actual position',actual_position)
+	if abs(actual_position.x - final_position.x) < 0.2 and abs(actual_position.y - final_position.y) < 0.2:
+	    	publisher.publish(command)
+	#print('actual position',actual_position)
 	rate.sleep() #added to respect the 10 hz rate
         
 
